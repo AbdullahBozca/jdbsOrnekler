6c91c7c9fea44d0571a2810c15436289f9173f96
Abdullah Bozca <abdullahbozca@gmail.com>
2022-01-09 00:01:35 +0300

    09/01/2022

---
+++ src/main/java/Jdbs1Query01.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class Jdbs1Query01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        // ilgili driveri yüklemelyiz
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // bağlantı oluşturmalıyız
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys?serverTimezone=UTC", "root", "123456");
+        // sql komutları için bir statement nesnesi oluşturur.
+        Statement st = con.createStatement();
+        //sql ifadeleri yazabilir ve çalıştırabiliriz
+        ResultSet veri = st.executeQuery("SELECT isim, maas FROM personel WHERE id=123456789");
+        // sonuçları aldık ve işledik
+        while (veri.next()) {
+            System.out.println(veri.getString("isim")+veri.getInt("maas"));
+        }
+        con.close();
+        st.close();
+        veri.close();
+
+    }
+}
